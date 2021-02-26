ccadf650bed5b20582259687b770a49140dc80ab
Nicolai Schütte <nicolai.schuette@googlemail>
2021-02-26 07:51:39 +0100

    Flatten module structure and install doctrine dbal

--- config/config.php
+++ config/config.php
@@ -13,24 +13,18 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
-    \Mezzio\Router\LaminasRouter\ConfigProvider::class,
-    \Laminas\Router\ConfigProvider::class,
-    \Laminas\HttpHandlerRunner\ConfigProvider::class,
-    \Laminas\Validator\ConfigProvider::class,
+    Mezzio\Router\LaminasRouter\ConfigProvider::class,
+    Laminas\Router\ConfigProvider::class,
+    Laminas\HttpHandlerRunner\ConfigProvider::class,
+    Laminas\Validator\ConfigProvider::class,
     // Include cache configuration
     new ArrayProvider($cacheConfig),
 
-    \Mezzio\Helper\ConfigProvider::class,
-    \Mezzio\ConfigProvider::class,
-    \Mezzio\Router\ConfigProvider::class,
-    \Laminas\Diactoros\ConfigProvider::class,
+    Mezzio\Helper\ConfigProvider::class,
+    Mezzio\ConfigProvider::class,
+    Mezzio\Router\ConfigProvider::class,
+    Laminas\Diactoros\ConfigProvider::class,
 
-    // Swoole config to overwrite some services (if installed)
-    class_exists(\Mezzio\Swoole\ConfigProvider::class)
-        ? \Mezzio\Swoole\ConfigProvider::class
-        : function(): array { return[]; },
-
-    // Default App module config
     App\ConfigProvider::class,
 
     // Load application config in a pre-defined order in such a way that local settings
@@ -40,9 +34,6 @@ $aggregator = new ConfigAggregator([
     //   - `local.php`
     //   - `*.local.php`
     new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
-
-    // Load development config if it exists
-    new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
 ], $cacheConfig['config_cache_path']);
 
 return $aggregator->getMergedConfig();
